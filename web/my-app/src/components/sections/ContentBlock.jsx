2010ae813d0e74a261e8cef69f2b31ebf1ca9dc6
Ahmad Wildan Syahputra <syahputraahmad707@gmail.com>
2025-12-12 18:03:43 +0700

    refactor ContentBlock component for improved data handling and styling

--- web/my-app/src/components/sections/ContentBlock.jsx
+++ web/my-app/src/components/sections/ContentBlock.jsx
@@ -6,23 +6,27 @@ import Sidebar from "./Sidebar";
  * Reusable content block: 2 MainSections + 1 CarouselStrip (left) + Sidebar (right)
  * This entire block can be repeated multiple times on the page
  */
-
 export default function ContentBlock({ 
   topMainSection,
   carouselItems,
   bottomMainSection,
   sidebar 
 }) {
+  // Defensive check: ensure all required data exists
+  if (!topMainSection || !bottomMainSection || !sidebar) {
+    return null;
+  }
+
   return (
-    <section className=" last:border-0">
+    <section className="">
       <div className="grid grid-cols-1 lg:grid-cols-12 gap-8">
         
         {/* LEFT COLUMN: MainSection + Carousel + MainSection */}
-        <div className="lg:col-span-8 space-y-8 lg:border-r lg:border-gray-200 lg:pr-8">
+        <div className="lg:col-span-8 space-y-8 lg:border-r lg:border-gray-300 lg:pr-8">
           {/* 1. MainSection Atas */}
           <MainSection 
-            sideTitles={topMainSection.sideTitles} 
-            featured={topMainSection.featured} 
+            sideTitles={topMainSection.sideTitles || []} 
+            featured={topMainSection.featured || {}} 
           />
 
           {/* 2. CarouselStrip */}
@@ -32,16 +36,16 @@ export default function ContentBlock({
 
           {/* 3. MainSection Bawah */}
           <MainSection 
-            sideTitles={bottomMainSection.sideTitles} 
-            featured={bottomMainSection.featured} 
+            sideTitles={bottomMainSection.sideTitles || []} 
+            featured={bottomMainSection.featured || {}} 
           />
         </div>
 
         {/* RIGHT COLUMN: Sidebar */}
         <div className="lg:col-span-4">
           <Sidebar 
-            mainArticle={sidebar.mainArticle} 
-            thumbArticles={sidebar.thumbArticles} 
+            mainArticle={sidebar.mainArticle || {}} 
+            thumbArticles={sidebar.thumbArticles || []} 
           />
         </div>
       </div>
