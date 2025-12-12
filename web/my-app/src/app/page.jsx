db1990e6777380a1f91c37f6ce1f3f71782e37f9
Ahmad Wildan Syahputra <syahputraahmad707@gmail.com>
2025-12-12 15:06:50 +0700

    refactor HomePage to use ContentBlock for dynamic content rendering

--- web/my-app/src/app/page.jsx
+++ web/my-app/src/app/page.jsx
@@ -1,64 +1,137 @@
 import Header from "../components/Header";
-import MainSection from "../components/sections/MainSection";
-import CarouselStrip from "../components/sections/CarouselStrip";
-import Sidebar from "../components/sections/Sidebar";
+import ContentBlock from "@/components/sections/ContentBlock";
 
-export default function Home() {
-  // Data constants (nanti fetch dari API)
-  const topFeatured = {
-    img: "/test.png",
-    href: "/international/international-summit",
-    priority: true, // For LCP optimization
-  };
-
-  const topSideTitles = [
+export default function HomePage() {
+  // Data untuk 3 blok (nanti akan diambil dari API backend)
+  const contentBlocks = [
+    // BLOK 1
     {
-      title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
-      excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Aenam Quis Magna In Urna Semper Lacinia...",
-      href: "/international/regional-security-talks",
+      id: 1,
+      topMainSection: {
+        sideTitles: [
+          {
+            title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
+            excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Aenam Quis Magna In Urna Semper Lacinia...",
+            href: "/international/article-1",
+          },
+          {
+            title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
+            excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras elementum libero ac...",
+            href: "/business/article-2",
+          },
+        ],
+        featured: {
+          img: "/test.png",
+          href: "/featured/1",
+          priority: true,
+        }
+      },
+      carouselItems: [
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+      ],
+      bottomMainSection: {
+        sideTitles: [
+          {
+            title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
+            excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Aenam Quis Magna In Urna Semper Lacinia...",
+            href: "/sports/article-1",
+          },
+          {
+            title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
+            excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras elementum libero ac...",
+            href: "/sports/article-2",
+          },
+        ],
+        featured: {
+          img: "/test.png",
+          href: "/featured/2",
+        }
+      },
+      sidebar: {
+        mainArticle: {
+          title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
+          img: "/test.png",
+          href: "/sidebar/main-1",
+        },
+        thumbArticles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+        ]
+      }
     },
+
+    // BLOK 2 (struktur sama, data berbeda)
     {
-      title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
-      excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras elementum libero ac...",
-      href: "/business/market-update",
+      id: 2,
+      topMainSection: {
+        sideTitles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 2.1...", href: "/article-2-1" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 2.2...", href: "/article-2-2" },
+        ],
+        featured: { img: "/test.png", href: "/featured/3" }
+      },
+      carouselItems: [
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+      ],
+      bottomMainSection: {
+        sideTitles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 2.3...", href: "/article-2-3" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 2.4...", href: "/article-2-4" },
+        ],
+        featured: { img: "/test.png", href: "/featured/4" }
+      },
+      sidebar: {
+        mainArticle: { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/sidebar/main-2" },
+        thumbArticles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+        ]
+      }
     },
-  ];
 
-  const stripItems = [
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/politics/policy-brief" },
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/health/health-update" },
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/tech/innovation" },
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/business/economy" },
-  ];
-
-  const bottomFeatured = {
-    img: "/test.png",
-    href: "/sports/featured-match",
-  };
-
-  const bottomSideTitles = [
+    // BLOK 3 (struktur sama, data berbeda)
     {
-      title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
-      excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Aenam Quis Magna In Urna Semper Lacinia...",
-      href: "/sports/match-analysis",
-    },
-    {
-      title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
-      excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Cras elementum libero ac...",
-      href: "/sports/player-interview",
-    },
-  ];
-
-  const sidebarMain = {
-    title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.",
-    excerpt: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit. Aenean quis magna in urna...",
-    img: "/test.png",
-    href: "/opinion/editorial",
-  };
-
-  const sidebarThumbs = [
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/national/update-1" },
-    { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/national/update-2" },
+      id: 3,
+      topMainSection: {
+        sideTitles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 3.1...", href: "/article-3-1" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 3.2...", href: "/article-3-2" },
+        ],
+        featured: { img: "/test.png", href: "/featured/5" }
+      },
+      carouselItems: [
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+        { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#" },
+      ],
+      bottomMainSection: {
+        sideTitles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 3.3...", href: "/article-3-3" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", excerpt: "Excerpt 3.4...", href: "/article-3-4" },
+        ],
+        featured: { img: "/test.png", href: "/featured/6" }
+      },
+      sidebar: {
+        mainArticle: { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "/sidebar/main-3" },
+        thumbArticles: [
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+          { title: "Lorem Ipsum Dolor Sit Amet, Consectetur Adipiscing Elit.", img: "/test.png", href: "#", date: "Senin, 9 Des 2025" },
+        ]
+      }
+    }
   ];
 
   return (
@@ -66,20 +139,15 @@ export default function Home() {
       <Header />
 
       <main className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8 py-8">
-        <div className="grid grid-cols-12 gap-8 relative">
-          {/* Left Column */}
-          <div className="col-span-12 lg:col-span-8">
-            <MainSection sideTitles={topSideTitles} featured={topFeatured} />
-            <CarouselStrip items={stripItems} />
-            <MainSection sideTitles={bottomSideTitles} featured={bottomFeatured} />
-          </div>
-
-          {/* Vertical divider */}
-          <div className="hidden lg:block absolute left-[calc(66.666%+0.3rem)] top-0 bottom-0 w-px bg-gray-200" />
-
-          {/* Right Sidebar */}
-          <Sidebar mainArticle={sidebarMain} thumbArticles={sidebarThumbs} />
-        </div>
+        {contentBlocks.map((block) => (
+          <ContentBlock
+            key={block.id}
+            topMainSection={block.topMainSection}
+            carouselItems={block.carouselItems}
+            bottomMainSection={block.bottomMainSection}
+            sidebar={block.sidebar}
+          />
+        ))}
       </main>
     </div>
   );
